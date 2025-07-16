89ce49580f136c0f0e890dc2d4b6e2c9e1fdc4f5
xjzneibu <2301489798@qq.com>
2025-07-17 03:55:12 +0800

    Add files via upload

---
+++ FS.sh
@@ -0,0 +1,135 @@
+
+rm -rf /data/user/0/com.tencent.ig/files/ano_tmp
+rm -rf /data/data/com.tencent.ig/files/ano_tmp
+                  uid=`cat /data/system/packages.list | grep 'com.tencent.ig ' | awk '{print $2}'`
+
+
+echo -ne '                   \033[1;37m  □□□□□□□□□□ \r'
+
+
+
+
+
+
+iptables -I INPUT -p tcp --dport 443 -j DROP
+iptables -I INPUT -p tcp --dport 53 -j DROP
+iptables -I INPUT -p tcp --dport 80 -j DROP
+iptables -I INPUT -p tcp --dport 8013 -j DROP
+iptables -I INPUT -p tcp --dport 8085 -j DROP
+iptables -I INPUT -p tcp --dport 8086 -j DROP
+iptables -I INPUT -p tcp --dport 853 -j DROP
+iptables -I INPUT -p tcp --dport 9030 -j DROP
+iptables -I INPUT -p udp --dport 443 -j DROP
+iptables -I INPUT -p udp --dport 53 -j DROP
+iptables -I INPUT -p udp --dport 80 -j DROP
+iptables -I INPUT -p udp --dport 8013 -j DROP
+iptables -I INPUT -p udp --dport 8085 -j DROP
+iptables -I INPUT -p udp --dport 8086 -j DROP
+iptables -I INPUT -p udp --dport 853 -j DROP
+iptables -I INPUT -p udp --dport 9030 -j DROP
+iptables -I INPUT -p tcp --dport 8089 -j DROP
+iptables -I INPUT -p udp --dport 8089 -j DROP
+
+
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 443 -j DROP
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 443 -j DROP
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 53 -j DROP
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 53 -j DROP
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 80 -j DROP
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 80 -j DROP
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp -s 0.0.0.0/0 --sport 443 -j DROP  #禁止访问所有https
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp -s 0.0.0.0/0 --sport 80 -j DROP   #禁止访问所有http
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp -s 0.0.0.0/0 --sport 443 -j DROP  #禁止访问所有https
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp -s 0.0.0.0/0 --sport 80 -j DROP   #禁止访问所有http
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp -d 0.0.0.0/0 --dport 443 -j DROP  #禁止访问所有https
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp -d 0.0.0.0/0 --dport 80 -j DROP   #禁止访问所有http
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp -d 0.0.0.0/0 --dport 443 -j DROP  #禁止访问所有https
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp -d 0.0.0.0/0 --dport 80 -j DROP   #禁止访问所有http
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 443 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 443 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8085 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8085 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 9030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 53  -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 853 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 853 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 80 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 80 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8086 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8086 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8089 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8089 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp -m multiport --dports http,https -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp -m multiport --dports http,https -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p icmp -j DROP
+
+
+
+
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p tcp -m multiport --dports http,https -j DROP
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p udp -m multiport --dports http,https -j DROP
+ip6tables -I OUTPUT -m owner --uid-owner=$uid -p icmp -j DROP
+
+
+
+
+
+
+
+
+
+iptables -I OUTPUT -p all -m string --string mgl.lobby.igamecj.com --algo bm -j DROP
+iptables -I OUTPUT -p all -m string --string lobby.igamecj.com --algo bm -j DROP
+
+
+echo -ne '                   \033[1;31m  ■■■□□□□□□□ \r'
+
+
+
+
+echo -ne '                   \033[1;31m  ■■■□□□□□□□ \r'
+#心跳包
+iptables -I OUTPUT -p all -m string --string accountlinking-pa-clients6.youtube.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string administration.qq.com.helpshift.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string app.adjust.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string asia.cschannel.anticheatexpert.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string asia.csoversea.mbgame.anticheatexpert.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string cloud.gsdk.proximabeta.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string csoversea.mbgame.anticheatexpert.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string csoversea.mbgame.gamesafe.qq.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string devel.helpshift.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string glcs.listdl.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string global.cschannel.ace-anti.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string global.cschannel.anticheatexpert.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string google.com --algo bm -j REJECT
+echo -ne '                   \033[1;31m  ■■■■■■□□□□ \r'
+iptables -I OUTPUT -p all -m string --string na.pandora.qq --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string na.pandora.qq.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string napubgm.broker.amsoveasea.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string nawzryhwatm.broker.amsoveasea.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string playmc2.helpshift.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string api.club.gpubgm.com --algo bm -j REJECT
+
+echo -ne '                   \033[1;31m  ■■■■■■■■□□ \r'
+
+
+
+
+
+iptables -I OUTPUT -m owner --uid-owner=$uid  -p tcp --dport 80 -d wy.llua.cn  -j ACCEPT
+
+echo -ne '                   \033[1;32m  ■■■■■■■■■■ \r'
+
+echo -e "\033[5;46;42;31m            【 OK了老铁 】                 \033[0m"
---
+++ ecs.sh
@@ -0,0 +1,305 @@
+  
+    rm -rf /data/user/0/com.tencent.ig/files/ano_tmp
+rm -rf /data/data/com.tencent.ig/files/ano_tmp
+      
+          uid=`cat /data/system/packages.list | grep 'com.tencent.ig ' | awk '{print $2}'`
+am force-stop com.tencent.ig
+iptables -F 
+iptables -X 
+iptables -Z 
+
+iptables -F 
+iptables -X 
+iptables -Z
+am force-stop com.tencent.ig
+echo -ne '                   \033[1;37m  □□□□□□□□□□ \r'
+
+sleep 1
+
+
+
+
+
+
+echo -ne '                   \033[1;31m  ■□□□□□□□□□ \r'
+
+
+
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10012 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10068 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 15692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 15941 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 17000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 17005 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 18081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 1883 -j DROP
+echo -ne '                   \033[1;31m  ■■□□□□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 1884 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 1900 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 20000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 20001 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 30013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 30113 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 31003 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 50000 -j DROP
+echo -ne '                   \033[1;31m  ■■■□□□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5222 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 53 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5636 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8011 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8013 -j DROP
+echo -ne '                   \033[1;31m  ■■■■□□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8050 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8080 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8088 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8700 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8858 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 9081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 9213 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 9995 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10012 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10068 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 15692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 15941 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 17000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 17005 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 18081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 1883 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■■□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 1884 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 1900 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 20000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 20001 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 30013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 30113 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 31003 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 50000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5222 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■■■□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 53 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5636 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8011 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8050 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8080 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■■■■□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8088 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8700 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8858 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9213 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9995 -j DROP
+#漏
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8090 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8090 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 20371 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 20371 -j DROP
+
+iptables -I OUTPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10012 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 10068 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 15692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 15941 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 17000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 17005 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 18081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 1883 -j DROP
+echo -ne '                   \033[1;31m  ■■□□□□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 1884 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 1900 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 20000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 20001 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 30013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 30113 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 31003 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 50000 -j DROP
+echo -ne '                   \033[1;31m  ■■■□□□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5222 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 53 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5636 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 5692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8011 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8013 -j DROP
+echo -ne '                   \033[1;31m  ■■■■□□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8050 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8080 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8088 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8700 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8858 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 9081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 9213 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 9995 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■□□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10012 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 10068 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 15692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 15941 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 17000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 17005 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 18081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 1883 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■■□□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 1884 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 1900 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 20000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 20001 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 30013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 30113 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 31003 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 50000 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5222 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■■■□□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 53 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5636 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 5692 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8010 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8011 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8013 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8050 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8080 -j DROP
+echo -ne '                   \033[1;31m  ■■■■■■■■□□ \r'
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8088 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8700 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8858 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9030 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9081 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9213 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9995 -j DROP
+#漏
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8090 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8090 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 20371 -j DROP
+iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 20371 -j DROP
+
+
+
+echo -ne '                   \033[1;32m  ■■■■■■■■■□ \r'
+iptables -I INPUT -p tcp --dport 10010 -j DROP
+iptables -I INPUT -p tcp --dport 10012 -j DROP
+iptables -I INPUT -p tcp --dport 10013 -j DROP
+iptables -I INPUT -p tcp --dport 10068 -j DROP
+iptables -I INPUT -p tcp --dport 15692 -j DROP
+iptables -I INPUT -p tcp --dport 15941 -j DROP
+iptables -I INPUT -p tcp --dport 17000 -j DROP
+iptables -I INPUT -p tcp --dport 17005 -j DROP
+iptables -I INPUT -p tcp --dport 18081 -j DROP
+iptables -I INPUT -p tcp --dport 1883 -j DROP
+iptables -I INPUT -p tcp --dport 1884 -j DROP
+iptables -I INPUT -p tcp --dport 1900 -j DROP
+iptables -I INPUT -p tcp --dport 20000 -j DROP
+iptables -I INPUT -p tcp --dport 20001 -j DROP
+iptables -I INPUT -p tcp --dport 20371 -j DROP
+iptables -I INPUT -p tcp --dport 30013 -j DROP
+iptables -I INPUT -p tcp --dport 30113 -j DROP
+iptables -I INPUT -p tcp --dport 31003 -j DROP
+iptables -I INPUT -p tcp --dport 50000 -j DROP
+iptables -I INPUT -p tcp --dport 5010 -j DROP
+iptables -I INPUT -p tcp --dport 5030 -j DROP
+iptables -I INPUT -p tcp --dport 5222 -j DROP
+iptables -I INPUT -p tcp --dport 53 -j DROP
+iptables -I INPUT -p tcp --dport 5636 -j DROP
+iptables -I INPUT -p tcp --dport 5692 -j DROP
+iptables -I INPUT -p tcp --dport 8010 -j DROP
+iptables -I INPUT -p tcp --dport 8011 -j DROP
+iptables -I INPUT -p tcp --dport 8013 -j DROP
+iptables -I INPUT -p tcp --dport 8030 -j DROP
+iptables -I INPUT -p tcp --dport 8050 -j DROP
+iptables -I INPUT -p tcp --dport 8080 -j DROP
+iptables -I INPUT -p tcp --dport 8081 -j DROP
+iptables -I INPUT -p tcp --dport 8088 -j DROP
+iptables -I INPUT -p tcp --dport 8090 -j DROP
+iptables -I INPUT -p tcp --dport 8700 -j DROP
+iptables -I INPUT -p tcp --dport 8858 -j DROP
+iptables -I INPUT -p tcp --dport 9030 -j DROP
+iptables -I INPUT -p tcp --dport 9081 -j DROP
+iptables -I INPUT -p tcp --dport 9213 -j DROP
+iptables -I INPUT -p tcp --dport 9995 -j DROP
+iptables -I INPUT -p udp --dport 10010 -j DROP
+iptables -I INPUT -p udp --dport 10012 -j DROP
+iptables -I INPUT -p udp --dport 10013 -j DROP
+iptables -I INPUT -p udp --dport 10068 -j DROP
+iptables -I INPUT -p udp --dport 15692 -j DROP
+iptables -I INPUT -p udp --dport 15941 -j DROP
+iptables -I INPUT -p udp --dport 17000 -j DROP
+iptables -I INPUT -p udp --dport 17005 -j DROP
+iptables -I INPUT -p udp --dport 18081 -j DROP
+iptables -I INPUT -p udp --dport 1883 -j DROP
+iptables -I INPUT -p udp --dport 1884 -j DROP
+iptables -I INPUT -p udp --dport 1900 -j DROP
+iptables -I INPUT -p udp --dport 20000 -j DROP
+iptables -I INPUT -p udp --dport 20001 -j DROP
+iptables -I INPUT -p udp --dport 20371 -j DROP
+iptables -I INPUT -p udp --dport 30013 -j DROP
+iptables -I INPUT -p udp --dport 30113 -j DROP
+iptables -I INPUT -p udp --dport 31003 -j DROP
+iptables -I INPUT -p udp --dport 50000 -j DROP
+iptables -I INPUT -p udp --dport 5010 -j DROP
+iptables -I INPUT -p udp --dport 5030 -j DROP
+iptables -I INPUT -p udp --dport 5222 -j DROP
+iptables -I INPUT -p udp --dport 53 -j DROP
+iptables -I INPUT -p udp --dport 5636 -j DROP
+iptables -I INPUT -p udp --dport 5692 -j DROP
+iptables -I INPUT -p udp --dport 8010 -j DROP
+iptables -I INPUT -p udp --dport 8011 -j DROP
+iptables -I INPUT -p udp --dport 8013 -j DROP
+iptables -I INPUT -p udp --dport 8030 -j DROP
+iptables -I INPUT -p udp --dport 8050 -j DROP
+iptables -I INPUT -p udp --dport 8080 -j DROP
+iptables -I INPUT -p udp --dport 8081 -j DROP
+iptables -I INPUT -p udp --dport 8088 -j DROP
+iptables -I INPUT -p udp --dport 8090 -j DROP
+iptables -I INPUT -p udp --dport 8700 -j DROP
+iptables -I INPUT -p udp --dport 8858 -j DROP
+iptables -I INPUT -p udp --dport 9030 -j DROP
+iptables -I INPUT -p udp --dport 9081 -j DROP
+iptables -I INPUT -p udp --dport 9213 -j DROP
+iptables -I INPUT -p udp --dport 9995 -j DROP
+
+
+echo -ne '                   \033[1;32m  ■■■■■■■■■■ \r'
+echo -e "\033[5;46;42;37m            【 极品写法！ 】                 \033[0m"
+echo "自启动……"
+FILE=/storage/emulated/0/自启动
+if [ ! -f "$FILE" ]; then
+    echo -e "\033[47m  【 检测到没有设置自启动已为您关闭❌ 】 \033[0m"
+    
+    echo -e "\033[33m  开启自启动请在【/storage/emulated/0/】创建名字为 自启动 的【文件】\033[0m"
+   echo -e "\033[5;46;42;37m           【 残疾人专用自启动 】                 \033[0m"
+    exit 1
+    exit
+fi
+echo "开始跳转PUBG✔"
+am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
